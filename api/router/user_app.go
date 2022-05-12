898d8d95700ee6fc6ce25a564b4f9bb0e03ec668
wuqinqiang <1185079673@qq.com>
2022-05-12 14:39:57 +0800

    feat:update router

--- api/router/user_app.go
+++ api/router/user_app.go
@@ -11,17 +11,17 @@ import (
 
 func RegisterUserApp(e *gin.RouterGroup) {
 	userAppHandler := handler.NewUserAppHandler(internal.NewServices().UserAppService)
-	g := e.Group("/user-app")
+	g := e.Group("/apply")
 	g.POST("", response.Json(userAppHandler.Apply))
 
 	authMiddleware, err := middleware.NewAuthMiddleware(
 		middleware.NewJWTMiddleware(internal.NewServices().UserAppService))
 	assert.CheckErr(err)
 
-	g.POST("/token/get", authMiddleware.LoginHandler)
-	g.GET("/token/refresh", authMiddleware.RefreshHandler)
+	g.POST("/token", authMiddleware.LoginHandler)
+	g.POST("/token/refresh", authMiddleware.RefreshHandler)
 
-	w := g.Group("/sub")
+	w := e.Group("/sub")
 	w.Use(authMiddleware.MiddlewareFunc())
 	w.POST("", response.Json(userAppHandler.AddSub))
 	w.DELETE("", response.Json(userAppHandler.CancelSub))
